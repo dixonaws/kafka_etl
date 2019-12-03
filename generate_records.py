28daeff0698b965a7474096cb1fe11ca38a3023c
dixonaws <[]May10!@#>
2019-12-03 11:40:58 +0100

    added arguments to generate_records.py

--- generate_records.py
+++ generate_records.py
@@ -1,36 +1,44 @@
 from record import Record
 import csv
 import sys
+import argparse
 
-str_aws_region = "eu-west-1"
+def main():
+	parser=argparse.ArgumentParser(description="Specify the number of records to generate and the filename to write")
 
-int_records_to_put = 100
-str_filename="employ_data.csv"
-list_records=[]
+	parser.add_argument("Records", help="The number of records to write")
+	parser.add_argument("Filename", help="The name of the file to write; if this file exists, then it will be overwritten.")
 
-file_employ_data=open(str_filename, "w")
+	args=parser.parse_args()
 
-sys.stdout.write("Generating " + str(int_records_to_put) + " records... ")
-for dict_record in Record().generate(int_records_to_put):  # generate int_users_to_put users
-	# print(record)
-	list_records.append(dict_record)
-print("done, list_records contains " + str(len(list_records)) + " records." )
+	int_records_to_put = int(args.Records)
+	str_filename=str(args.Filename)
 
-sys.stdout.write("Writing " + str_filename + " in CSV format... ")
-csvwriter=csv.writer(file_employ_data)
+	list_records=[]
 
-# write each record in the list
-int_counter=0
-for dict_record in list_records:
-	# if we are writing the first record, write the header row
-	if(int_counter==0):
-		csvwriter.writerow(dict_record.keys())
+	file_employ_data=open(str_filename, "w")
 
-	csvwriter.writerow(dict_record.values())
-	int_counter=int_counter+1
+	sys.stdout.write("Generating " + str(int_records_to_put) + " records... ")
+	for dict_record in Record().generate(int_records_to_put):  # generate int_users_to_put users
+		# print(record)
+		list_records.append(dict_record)
+	print("done, list_records contains " + str(len(list_records)) + " records." )
 
-print("done.")
-file_employ_data.close()
+	sys.stdout.write("Writing " + str_filename + " in CSV format... ")
+	csvwriter=csv.writer(file_employ_data)
 
+	# write each record in the list
+	int_counter=0
+	for dict_record in list_records:
+		# if we are writing the first record, write the header row
+		if(int_counter==0):
+			csvwriter.writerow(dict_record.keys())
 
+		csvwriter.writerow(dict_record.values())
+		int_counter=int_counter+1
+
+	print("done.")
+	file_employ_data.close()
+
+main()
 
