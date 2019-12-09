21bf2307df85445ecf42cc89f4955a5e8b515358
dixonaws <[]May10!@#>
2019-12-09 11:36:21 +0100

    improvements to process_csv.py to get a file from S3

--- process_csv.py
+++ process_csv.py
@@ -1,5 +1,7 @@
 import csv
 from kafka import KafkaProducer
+import sys
+import boto3
 
 # return a list containing one record for each line in a CSV file
 def read_file(str_filename):
@@ -12,6 +14,18 @@ def read_file(str_filename):
     file_filename.close()
     return(lst_file_contents)
 
+def read_file_from_s3(str_s3_bucket, str_s3_key):
+    lst_file_contents=[]
+    s3_resource=boto3.resource("s3")
+
+    file=s3_resource.Object(str_s3_bucket, str_s3_key)
+
+    str_file=file.get()["Body"].read().decode("utf-8")
+
+    lst_file_contents=str_file.splitlines()
+
+    return(lst_file_contents)
+
 # insert into the kafka topic buffer (don't flush)
 def insert_into_kafka_topic(kafka_producer, str_topic_name, str_message):
     bytes_message=bytes(str_message, "utf-8")
@@ -21,18 +35,27 @@ def insert_into_kafka_topic(kafka_producer, str_topic_name, str_message):
 def main():
     str_filename="records.csv"
     str_bootstrap_servers = "localhost:9092"
+    str_s3_bucket="dixonaws-test"
+
     kafka_producer = KafkaProducer(bootstrap_servers=str_bootstrap_servers)
 
-    lst_records=read_file(str_filename)
+    #lst_records=read_file(str_filename)
+
+    lst_records=read_file_from_s3(str_s3_bucket, str_filename)
 
     print("lst_records contains " + str(len(lst_records)) + " records")
 
     str_topic_name="test"
 
     # insert each record into the kafka topic buffer
+    sys.stdout.write("Inserting into topic " + str_topic_name)
+    int_i=1
     for str_record in lst_records:
-        print(type(str_record))
+        int_i=int_i+1
+        sys.stdout.write(".")
         insert_into_kafka_topic(kafka_producer, str_topic_name, str_record)
+
+    print(" done (" + str(int_i) + " records processed).")
 
     # finally write to the buffer (flush the buffer to kafka all at once)
     kafka_producer.flush()
